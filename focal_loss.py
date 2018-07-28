5f87ef153e70ccb7fd1f03c341c0a8fcb475174e
Shuhao Yuan <294663908@qq.com>
2018-07-28 10:47:51 +0800

    add epsilon to prevent NaN error

--- focal_loss.py
+++ focal_loss.py
@@ -10,14 +10,15 @@ def focal_loss_softmax(labels,logits,gamma=2):
       logits: A float32 tensor of shape [batch_size,num_classes].
       gamma: A scalar for focal loss gamma hyper-parameter.
     Returns:
-      A tensor of the same shape as `lables`
+      A tensor of the same shape as `labels`
     """
+    epsilon=1.e-8
     y_pred=tf.nn.softmax(logits,dim=-1) # [batch_size,num_classes]
     y=tf.one_hot(labels,depth=y_pred.shape[1])  #output size [batch_size,num_classes]
 
-    L1=-y*((1-y_pred)**gamma)*tf.log(y_pred)  # select data with y=1
+    L1=-y*((1-y_pred)**gamma)*tf.log(y_pred+epsilon)  # select data with y=1
 
-    L2=-(1-y)*(y_pred**gamma)*tf.log(1-y_pred) # select data with y=0
+    L2=-(1-y)*(y_pred**gamma)*tf.log(1-y_pred+epsilon) # select data with y=0
 
     L=L1+L2
 
