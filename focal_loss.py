31c3f2eeaf068bd7555cdf131e5cf09a5242a334
Shuhao Yuan <294663908@qq.com>
2018-08-30 18:52:01 +0800

    update because the dim is deprecated

--- focal_loss.py
+++ focal_loss.py
@@ -13,7 +13,7 @@ def focal_loss_softmax(labels,logits,gamma=2):
       A tensor of the same shape as `labels`
     """
     epsilon=1.e-8
-    y_pred=tf.nn.softmax(logits,dim=-1) # [batch_size,num_classes]
+    y_pred=tf.nn.softmax(logits,axis=-1) # [batch_size,num_classes]
     y=tf.one_hot(labels,depth=y_pred.shape[1])  #output size [batch_size,num_classes]
 
     L1=-y*((1-y_pred)**gamma)*tf.log(y_pred+epsilon)  # select data with y=1
